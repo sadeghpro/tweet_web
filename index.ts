6620ce7702746f93aabcc213a9c10e9ba4c17e04
sadeghpro <sadeghpro91@gmail.com>
2023-12-05 15:31:40 +0330

    Create test file

--- index.ts
+++ index.ts
@@ -1,16 +1,5 @@
 import TweetWeb from "./src/TweetWeb";
 import Tweet from "./src/Tweet";
 
-(async () => {
-    const tweetWeb = await TweetWeb.create({
-        args: ['--no-sandbox'],
-        executablePath: "/usr/bin/google-chrome",
-        headless: false,
-    })
-    console.log(await tweetWeb.getPost('https://x.com/notelonmuskkkk/status/1728682129289040278', 20000));
-    console.log(await tweetWeb.getPost('https://twitter.com/H_umdard/status/1728683855044063363'));
-})();
-
-
 export default TweetWeb;
 export {Tweet};
---
+++ test.ts
@@ -0,0 +1,11 @@
+import TweetWeb from "./dist";
+
+(async () => {
+    const tweetWeb = await TweetWeb.create({
+        args: ['--no-sandbox'],
+        executablePath: "/usr/bin/google-chrome",
+        headless: false,
+    })
+    console.log(await tweetWeb.getPost('https://x.com/notelonmuskkkk/status/1728682129289040278', 20000));
+    console.log(await tweetWeb.getPost('https://twitter.com/H_umdard/status/1728683855044063363'));
+})();
